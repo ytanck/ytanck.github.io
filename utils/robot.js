d9ea71597629232230f7a8e7cef2cc9070383aee
ytking <69585768+ytking@users.noreply.github.com>
2023-11-06 09:22:06 +0000

    Deploying to master from @ ytking/blog_admin@921603c0bf9d7f07758b6ccd3e91dde9f5a909c0 🚀

---
+++ utils/robot.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+
+function dingtalk(content) {
+  return axios({
+    method: 'post',
+    url: 'https://oapi.dingtalk.com/robot/send?access_token=88218abc4700cc2c0b4646de7cbd7db37cf3b96acce62484591d9c57ae9ba420',
+    data: {
+      msgtype: 'text',
+      text: {
+        content,
+      }
+    },
+  })
+}
+dingtalk('又到周五了，记得发日报哦')
+
