a95ebb64f4ac57f90e9b4679f385c0fbc3cd91ab
ytanck <69585768+ytanck@users.noreply.github.com>
2023-12-19 06:57:17 +0000

    Deploying to master from @ ytanck/blog_admin@123e88cce5d549cc8e893f51e4980df90142e37d 🚀

--- utils/robot.js
+++ utils/robot.js
@@ -2,8 +2,8 @@ const axios = require('axios')
 function dingtalk(content) {
   return axios({
     method: 'post',
-    // url: 'https://oapi.dingtalk.com/robot/send?access_token=88218abc4700cc2c0b4646de7cbd7db37cf3b96acce62484591d9c57ae9ba420',// 测试
-    url: 'https://oapi.dingtalk.com/robot/send?access_token=fbad06cb691dfbd2ba54595e533c34cada047d32df95966903fcaddbcf48d702',// 前端群
+    url: 'https://oapi.dingtalk.com/robot/send?access_token=88218abc4700cc2c0b4646de7cbd7db37cf3b96acce62484591d9c57ae9ba420',// 测试
+    // url: 'https://oapi.dingtalk.com/robot/send?access_token=fbad06cb691dfbd2ba54595e533c34cada047d32df95966903fcaddbcf48d702',// 前端群
     data: {
       msgtype: 'text',
       text: {
